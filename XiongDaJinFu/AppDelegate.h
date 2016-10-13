bc584a48baed6a85942a9e7184a6e3a09a840fdc
码动 <madong@madongdeimac.local>
2016-10-13 10:34:19 +0800

    Initial Commit

---
+++ XiongDaJinFu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiongDaJinFu
+//
+//  Created by 码动 on 16/10/13.
+//  Copyright © 2016年 digirun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
